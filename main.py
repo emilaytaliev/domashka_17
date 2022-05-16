ebb00ca6113eb09f112729622f83031f1e7e6adf
Эмиль <emil.aytaliev@mail.ru>
2022-05-16 21:09:14 +0300

    1

---
+++ main.py
@@ -0,0 +1,91 @@
+from flask import Flask, request
+from flask_restx import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import Schema, fields
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+api = Api(app)
+
+movie_ns = api.namespace('movies')
+director_ns = api.namespace('directors')
+genre_ns = api.namespace('genres')
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    trailer = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director = db.relationship("Director")
+
+class Director(db.Model):
+    __tablename__ = 'director'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
+class DirectorSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+
+class GenreSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+
+@movie_ns.route('/')
+class MovieView(Resource):
+    def get(self):
+        director_id = request.args.get('director_id')
+        genre_id = request.args.get('genre_id')
+        movies = Movie.query
+        if director_id:
+            movies = Movie.query.filter(Movie.director_id == director_id)
+        elif genre_id:
+            movies = Movie.query.filter(Movie.genre_id == genre_id)
+        else:
+            movies = Movie.query.all()
+        return movies_schema.dump(movies)
+
+
+@movie_ns.route('/<int:id>')
+class MovieView(Resource):
+    def get(self, id):
+        m = Movie.query.get(id)
+        return movie_schema.dump(m)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
